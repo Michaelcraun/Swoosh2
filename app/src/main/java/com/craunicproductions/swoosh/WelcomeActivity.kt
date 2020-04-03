547f05575bccae2abdf5db9dc48d0e602892d95d
Michael <michael.craun@gmail.com>
2020-04-02 20:49:26 -0400

    Initial commit

---
+++ app/src/main/java/com/craunicproductions/swoosh/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.craunicproductions.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
